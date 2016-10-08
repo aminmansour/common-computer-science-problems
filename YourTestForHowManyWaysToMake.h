ead29c63387a26a952f6f255a0c3c469e838b862
Amin mnasour <ama>
2016-10-08 15:42:56 +0100

    have created test for part a before attempting it

--- YourTestForHowManyWaysToMake.h
+++ YourTestForHowManyWaysToMake.h
@@ -2,12 +2,24 @@
 #define YOUR_TEST_FOR_HOW_MANY_WAYS_TO_MAKE_H
 
 #include "HowManyWaysToMake.h"
-
+#include <iostream>
+#include <vector>
+using std::cout;
+using std::vector;
 
 int testHowManyWaysToMake() {
     // TODO: your test case for question 1b goes inside this function, instead of the next line of code:
-    return 0;
-    
+    vector<int> test ={2,4,10};
+    int amount = howManyWaysToMake(20,test);
+    if(amount == 10){
+      cout << "The code passed: code correctly returns that there are 10 ways to make 20 out of {2,4,10}\n";
+      return 0;
+    }
+    else{
+      cout << "The code failed: code instead returns " << amount << " ways to make 20 out of {2,4,10} -- but the answer is 10\n";
+      return 1;
+    }
+
 }
 
 
